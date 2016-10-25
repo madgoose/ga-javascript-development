237c2626df3b6d54feeb5cff8a5a88f3792f359d
madgoose <dominic.hey@gmail.com>
2016-10-25 23:48:28 +0100

    implement basic templating engine using for loop to iterate through data object

--- week05/custom-template-engine/scripts/script.js
+++ week05/custom-template-engine/scripts/script.js
@@ -7,24 +7,26 @@ data.myDetails = {
 };
 
 $(function(){
+	
+	// declare local variables
+	var $template = $("#my-custom-template").html();
+
 	// use regex to target double curly parentheses + key values from data object
 	function convertTemplate(templateString, values){
+
 		var regexMatch = /\{\{([a-zA-Z]*)\}\}/g; // g at end means global - it will not stop after the first instance
 		var matches = templateString.match(regexMatch);
+		
 		for(var i = 0; i < matches.length; i++){
 			var key = matches[i].replace(/{{/g, '').replace(/}}/g, '');
 			var correspondingValue = values[key];
-			console.log(key, correspondingValue);
 			templateString = templateString.replace(key, correspondingValue);
-			//console.log(templateString);
-
-			// use regex to remove curly braces, then write to DOM
+			templateString = templateString.replace(/{{/g, '').replace(/}}/g, '');
 		};
-		//console.log(matches);
+
+		// write to DOM
+		$("body").append(templateString);
 	};
 
-	// get custom template
-	var $template = $("#my-custom-template").html();
 	convertTemplate($template, data.myDetails);
-	$("body").append($template);
 });
