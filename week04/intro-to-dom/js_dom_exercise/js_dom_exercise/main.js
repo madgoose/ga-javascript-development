bdff228157808be2a84a5799e080eceaba222c59
madgoose <dominic.hey@gmail.com>
2016-10-26 12:54:27 +0100

    jquery from google cdn with local fallback for developing without internet

--- week04/intro-to-dom/js_dom_exercise/js_dom_exercise/main.js
+++ week04/intro-to-dom/js_dom_exercise/js_dom_exercise/main.js
@@ -9,20 +9,45 @@ Making a favorites list: DOM manipulation
 - Also, when a new item is added to the list, clear the input box.
 
 */
-MyApp = {};
+/*MyApp = {};
 
 MyApp.btnCreate = document.getElementById("new-thing-button");
 
-MyApp.addToList = function(list, newThing){
-	console.log("something");
+window.onload = function() {
+  
+  // when someone clicks the button...
+  MyApp.btnCreate.onclick = function(){
+  	//console.log("button clicked");
+  	MyApp.addToList("word", "thing");
+  }
+
+  MyApp.addToList = function(list, newThing){
+    console.log(list, newThing);
+  }
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+};*/
+
+// =============== //
+
+/* Independent Practice
+
+Making a favorites list: DOM manipulation
+
+
+- When the user clicks the submit button, take the value they've typed 
+  into the input box and add it to the list (remember: appendChild)
+
+- Also, when a new item is added to the list, clear the input box.  
+
+*/
+
+function addToList(list, newThing) {
+
 }
 
 window.onload = function() {
   // when someone clicks the button...
-  MyApp.btnCreate.onclick = function(){
-  	console.log("button clicked");
-  	return false;
-  }
 
   // https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
 };
@@ -31,7 +56,7 @@ window.onload = function() {
 
 Bonus:
 
-When they click submit without typing anything, alert the user
+When they click submit without typing anything, alert the user 
 "you must type in a value!"
   (Hint: the `value` property of the input box, before anyone types in it,
   is the empty string.)
