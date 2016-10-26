6f5d5319e9fc1c206d17551ac3b885a63aa8d829
madgoose <dominic.hey@gmail.com>
2016-10-26 21:46:09 +0100

    refactor template convertTemplate

--- week05/custom-template-engine/scripts/script.js
+++ week05/custom-template-engine/scripts/script.js
@@ -7,27 +7,27 @@ data.myDetails = {
 };
 
 $(function(){
-	
+
 	// declare local variables
 	var $template = $("#my-custom-template").html();
-	
+
 	function convertTemplate(templateString, values){
 
 		// use regex to target double curly parentheses + key-value pairs from data.myDetails object
 		var regexMatch = /\{\{([a-zA-Z]*)\}\}/g; // g at end means global - it will not stop after the first instance
-		
+
 		// new array containing strings that match regex expression
 		var matches = templateString.match(regexMatch);
-		
+
 		// iterate over array
-		matches.map(function(key){
+		matches.map(function(key, index){
 
 			// replace double parentheses with empty string
 			key = key.replace(/{{/g, '').replace(/}}/g, '');
 			// assign value to variable
 			var correspondingValue = values[key];
 			// replace key with value
-			templateString = templateString.replace(key, correspondingValue).replace(/{{/g, '').replace(/}}/g, '');
+			templateString = templateString.replace(matches[index], correspondingValue)
 		});
 
 		// write to DOM
