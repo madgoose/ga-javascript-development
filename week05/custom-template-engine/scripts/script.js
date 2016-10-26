333b931890c090d8712cdd131bd136e0daf73bd7
madgoose <dominic.hey@gmail.com>
2016-10-26 10:55:14 +0100

    tidy up script.js and add some comments

--- week05/custom-template-engine/scripts/script.js
+++ week05/custom-template-engine/scripts/script.js
@@ -10,13 +10,16 @@ $(function(){
 	
 	// declare local variables
 	var $template = $("#my-custom-template").html();
-
-	// use regex to target double curly parentheses + key values from data object
+	
 	function convertTemplate(templateString, values){
 
+		// use regex to target double curly parentheses + key-value pairs from data.myDetails object
 		var regexMatch = /\{\{([a-zA-Z]*)\}\}/g; // g at end means global - it will not stop after the first instance
+		
+		// new array containing strings that match regex expression
 		var matches = templateString.match(regexMatch);
 		
+		// iterate over array
 		matches.map(function(key){
 
 			key = key.replace(/{{/g, '').replace(/}}/g, '');
