01671abf5d6bbbfe9f1bfdf9069efa85d67e5408
madgoose <dominic.hey@gmail.com>
2016-10-27 17:58:23 +0100

    add external data file to custom-template-engine

---
+++ week05/custom-template-engine/scripts/data.js
@@ -0,0 +1,35 @@
+var data = {};
+data.myDetails = {
+  firstName: "Bob",
+  lastName: "Hope",
+  birthDate: "30 December 1856"
+}
+
+data.jobs = [
+  {
+    companyName: "Entropic Synergies",
+    startDate: "10/09/2009",
+    endDate: "10/09/2010",
+    jobTitle: "Junior developer"
+  },
+  {
+    companyName: "Le Wattman",
+    startDate: "23/11/2010",
+    endDate: "16/12/2012",
+    jobTitle: "Developer"
+  },
+  {
+    companyName: "My Startup",
+    startDate: "22/02/2013",
+    endDate: "22/05/2014",
+    jobTitle: "Lead developer"
+  },
+  {
+    companyName: "General Assembly",
+    startDate: "22/07/2014",
+    endDate: "----",
+    jobTitle: "Faculty Chair"
+  }
+];
+
+window.data = data;
