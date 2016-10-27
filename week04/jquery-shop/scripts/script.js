e1457d5063f8b6d03e9ad452a169dab4b52eaf8a
madgoose <dominic.hey@gmail.com>
2016-10-27 17:44:02 +0100

    complete jQuery Shop homework assignment

--- week04/jquery-shop/scripts/script.js
+++ week04/jquery-shop/scripts/script.js
@@ -1,12 +1,45 @@
 $(function() {
 	
 	// local variables
-	var $cart = $("#shopping-cart");
+	var $shoppingCart = $("#shopping-cart");
 	var $totalPrice = $("#total");
 	var $buttons = $(".buy-item");
 	var currentTotal = parseInt($totalPrice.html());
 
-	// add click event handler to all elements with "buy-item" class 
+	var updateCartPrice = function(productPrice) {
+		
+		// update price
+		currentTotal = currentTotal + productPrice;
+		
+		// write to DOM
+		$totalPrice.html(currentTotal);
+
+	};
+
+	var updateCartList = function(productPrice, productTitle) {
+				
+		// write to DOM
+		$shoppingCart.append("<li>"+productTitle+" (&pound;<span>"+productPrice+"</span>)<button class=\"remove-item\">remove item</button></li>");
+
+	};
+
+	var removeListItem = function(itemToRemove){
+		itemToRemove.remove();
+	};
+
+	// add click event handler to all ".remove-item" buttons
+	$shoppingCart.on( "click", ".remove-item", function() {
+		
+		// get this li element
+		var itemToRemove = $(this).parent().remove();
+		// get value from adjacent span element
+		var productPrice = parseInt($(this).siblings("span").html());
+		
+		removeListItem(itemToRemove);
+		updateCartPrice(productPrice * -1); // multiply by -1 to create negative version of number
+	});
+
+	// add click event handler to all ".buy-item" buttons 
 	$buttons.click(function() {
 		
 		// get string values of .product-title and .price sibling elements
@@ -14,14 +47,9 @@ $(function() {
 		var productPrice = parseInt($(this).siblings(".price").html());
 		
 		// function call
-		updateCart(productTitle, productPrice);
-	});
+		updateCartPrice(productPrice);
+		updateCartList(productPrice, productTitle);
 
-	var updateCart = function(productTitle, productPrice) {
-		
-		currentTotal = currentTotal + productPrice;
-		$totalPrice.html(currentTotal);
-		//console.log(currentTotal);
-	};
+	});
 
 });
