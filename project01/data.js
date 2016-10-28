5e65df44a64cf12bd0ccb44a759134905881e8e4
madgoose <dominic.hey@gmail.com>
2016-10-28 22:32:18 +0100

    initial pseudo code and thoughts for data model, programming logic/classes, and html/DOM structure

---
+++ project01/data.js
@@ -0,0 +1,264 @@
+var data = {};
+
+data.cards = [
+	{
+		value: "1",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "2",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "3",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "4",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "5",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "6",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "7",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "8",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "9",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "Jack",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "Queen",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "King",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "Ace",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "1",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "2",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "3",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "4",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "5",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "6",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "7",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "8",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "9",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "Jack",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "Queen",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "King",
+		suite: "Diamonds",
+		symbol: "♦"
+	},
+	{
+		value: "Ace",
+		suite: "Diamonds",
+		symbol: "♣"
+	},
+	{
+		value: "1",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "2",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "3",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "4",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "5",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "6",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "7",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "8",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "9",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "Jack",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "Queen",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "King",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "Ace",
+		suite: "Hearts",
+		symbol: "♥"
+	},
+	{
+		value: "1",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "2",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "3",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "4",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "5",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "6",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "7",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "8",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "9",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "Jack",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "Queen",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "King",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "Ace",
+		suite: "Spades",
+		symbol: "♠"
+	}
+];
--- project01/notes.md
+++ project01/notes.md
@@ -1,31 +1,73 @@
-#things and actions required for turn-based games
+#Things and actions required for turn-based games
 
-##blackjack
+##Blackjack
 
 Make a one player game where people down on their luck can lose all their money by guessing which card the computer will deal next!
+
+###Data
+```
+var data = {};
+
+data.cards = [
+	{
+		value: "1",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "2",
+		suite: "Clubs",
+		symbol: "♣"
+	},
+	{
+		value: "King",
+		suite: "Spades",
+		symbol: "♠"
+	},
+	{
+		value: "Ace",
+		suite: "Spades",
+		symbol: "♠"
+	}
+	];
+```
+
 ###Class design
+
 - Game // object
 	- Player // object
 		- name // string
-		- wallet // number
-			- if wallet <= 0 then endGame
-		- makeGuess() // method
-			- setAmount() // method
-				- if amount > wallet then no bet
+		- credits // number: 1 credit = £1
+			- if credits <= 0 then endGame
+		- guessCard() // method
 			- setGuess() // method
-				- 
+				- pick a number
+				- pick a suit
+			- setWager() // method
+				- if wager > wallet then no bet
 		- cashOut() // method: endsGame + winner message
-	- CardDeck // object
-		- cards // array 
-		- card // object
-			- suite // string
-			- value // number
-				- replace() to output jack/queen/king instead of 10, 11, 12…
-		- getRandomCard() // method: select random index value of cards array
-		- turnCard() // method: calls getRandomCard and pops the return value from CardDeck.cards and pushes to Board.discardedCards
+	- Card // object defined in external JSON data file
+		- suite // string
+		- value // string
+		- symbol // string (unicode character)
+	- Cards // array of
+		- getRandomCard() // method: pop the return value from Board.newCards and push to Board.oldCards
 	- Board // object
-		- CardDeck.cards // 
-		- discardedCards // array
-		- displayScore() // method
-		- displayCard() // method
+		- newCards // array: data.cards.map()
+		- oldCards // array: initialised empty
+		- currentCard // number 0-51
+		- updateScore() // method
+		- updateCard() // method: CardDeck.getRandomCard
 	- endGame() // method
+
+###Presentation layer
+- html structure with handlebars.js templating
+	- eg. html > body
+		- div#player01.player
+			- p.name {{name}}
+			- p.credits £{{credits}}
+		- div#board
+			- div.current-card
+				- div.card.card01 … .card52
+- modular css to style the html
+
