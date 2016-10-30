7d211d3176dd25a947dd62995695bc1f9c4384dc
madgoose <dominic.hey@gmail.com>
2016-10-30 13:39:41 +0000

    map card data from external JSON file to local array

--- project01/scripts/app.js
+++ project01/scripts/app.js
@@ -1,14 +1,17 @@
+// new Game constructor
 var Game = function(gameName, instructions){
-	this.gameName = gameName,
-	this.instructions = instructions
+	this.gameName = gameName;
+	this.instructions = instructions;
 };
 
+// display game name and instructions
 Game.prototype.introduceGame = function() {
 	console.log("Welcome to " + this.gameName + "!\n" + this.instructions);
 };
 
+// display game end message
 Game.prototype.endGame = function() {
-	console.log("Game over!");
+	console.log("Game over!\nBad luck +playerNameToBeAddedLater+");
 };
 
 // set credits
@@ -26,7 +29,30 @@ Game.prototype.getCredits = function(){
 	return this.credits;
 };
 
+// populate newCards array with data from external JSON
+// i.e. deal a new hand
+Game.prototype.setNewCards = function() {
+	
+	// populate new array with 
+	this.newCards = gameData.cards.map(function(arrayItem){
+		this.value = arrayItem.value;
+		this.suite = arrayItem.suite;
+		this.symbol = arrayItem.symbol;
+		return arrayItem;
+	});
+
+	return this.newCards;
+};
+
+// get credits
+Game.prototype.getNewCards = function(){
+	return this.newCards;
+};
+
+
+// construct new Game instance, assign to local variable hitorbust
 var hitorbust = new Game(gameData.gameName, gameData.instructions);
 
 // newCredits value will eventually be taken from text input on game interface
 hitorbust.setCredits(gameData.startingCredit);
+hitorbust.setNewCards();
